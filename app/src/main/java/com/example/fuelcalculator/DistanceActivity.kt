4385ced5249f368aad43f9734404f0043794afa0
chris <ramoschris901@gmail.com>
2024-12-18 17:20:55 +0000

    Modifcações de telas e começando a logica

--- app/src/main/java/com/example/fuelcalculator/DistanceActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/DistanceActivity.kt
@@ -4,48 +4,35 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
-class Price_fuelActivity : AppCompatActivity() {
+class DistanceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_price_fuel)
+        setContentView(R.layout.activity_distance)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val edtcombustivel = findViewById<TextInputEditText>(R.id.edt_combustivel)
-        val btnproximo1 = findViewById<Button>(R.id.btnproximo1)
 
-        btnproximo1.setOnClickListener {
-            val combustivel: String = edtcombustivel.text.toString()
+        val edtdistance = findViewById<TextInputEditText>(R.id.edtdistance)
+        val btnproximo3 = findViewById<Button>(R.id.btnproximo3)
 
-            if (edtcombustivel.text.toString().isEmpty())
+        btnproximo3.setOnClickListener {
+            val edtdistance: String = edtdistance.text.toString()
 
-            Snackbar.make(
-                edtcombustivel, "Preencha todos os campos ",
-                Snackbar.LENGTH_LONG
-            )
-                .show()
+            val intent = Intent(this, ResultActivity::class.java)
 
-
-
-
-            val intent = Intent(this, Consumer_FuelActivity::class.java)
             startActivity(intent)
         }
 
 
     }
 
-
 }
-
--- app/src/main/java/com/example/fuelcalculator/ResultActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/ResultActivity.kt
@@ -1,36 +1,20 @@
 package com.example.fuelcalculator
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputEditText
 
-class Consumer_FuelActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_consumer_fuel)
+        setContentView(R.layout.activity_result)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val edtconsumer = findViewById<TextInputEditText>(R.id.edtconsumer)
-        val btnproximo2 = findViewById<Button>(R.id.btnproximo2)
-
-        btnproximo2.setOnClickListener {
-            val consumer: String = edtconsumer.text.toString()
-
-
-            val intent = Intent(this, DistanceActivity::class.java)
-            startActivity(intent)
-        }
-
-
     }
 }
